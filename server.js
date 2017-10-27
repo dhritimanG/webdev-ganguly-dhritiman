0d681b8a714f17d4e2b6aa81bcae1e9970ee5b7c
dhritimanG <dhritiman.ganguly@outlook.com>
2017-10-26 22:58:16 -0400

    app.js modify

--- server.js
+++ server.js
@@ -1,9 +1,3 @@
-/**
- * Created by sesha on 6/2/17.
- */
-
-// Get the dependencies
-
 const express = require('express');
 const path = require('path');
 const http = require('http');
@@ -13,13 +7,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
 // Point static path to dist -- For building -- REMOVE
 app.use(express.static(path.join(__dirname, 'dist')));
-
-
 
 // CORS
 app.use(function(req, res, next) {
@@ -29,9 +18,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-
-
-
 const port = process.env.PORT || '3100';
 app.set('port', port);
 
@@ -39,14 +25,14 @@ app.set('port', port);
 // Create HTTP server
 const server = http.createServer(app);
 
-var serverSide = require("./server/test-mongodb/app");
-serverSide(app);
+// var serverSide = require("./server/test-mongodb/app");
+// serverSide(app);
 
-require("./assignment/app")(app);
+require("./assignment/app.js")(app);
 
 // For Build: Catch all other routes and return the index file -- BUILDING
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
 
